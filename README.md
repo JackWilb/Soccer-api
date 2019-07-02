5197211f7ce205fcfa2df71ec768eac9f21261e2
JackWilb97 <jackwilburn@tutanota.com>
2019-07-02 14:36:20 -0600

    Update documentation and comments

--- README.md
+++ README.md
@@ -9,10 +9,33 @@ This project contains a python flask backend and a javascript/html/css frontend.
 
 To set up this project there are a couple steps:
 
-* Navigate into the backend folder
+* Clone the repo with `git clone https://github.com/JackWilb/Soccer-api.git`.
 * Make sure you have python3 installed. 
-* Optionally intialize a virtualenv with python3. 
+* Make sure you have node and angular8 installed on your machine.
+* Navigate into the backend folder
+* Optionally intialize a virtualenv with python3 with `virtualenv -p python3 soccerapi` and then run `source soccerapi/bin/activate`. 
 * Install the package requirements by running `pip install -r requirement.txt`.
 * Run `python app.py` to start the flask server.
 * Navigate to the frontend folder.
+* run `npm install`.
+* Run `ng build` and then `ng serve`.
+* Go to your browser and open up both: 127.0.0.1:5000/players/ and 127.0.0.1:4200.
+
+## 3. If it doesn't run
+
+If it's not running on your machine withmy instructions you can also access the server with my ip address (shared seperately).
+
+## 4. API Demonstration
+
+The api has 5 different endpoints that serve data. Here are the links that will generate json for the 4 that don't accept input:
+
+* 127.0.0.1:5000/players/
+* 127.0.0.1:5000/countries/
+* 127.0.0.1:5000/clubs/
+* 127.0.0.1:5000/attributes/
+
+Here is the one endpoint that changes (note that the capitalization is important and the names shouldn't have spaces):
+
+* 127.0.0.1:5000/players/CristianoRonaldo
+* 127.0.0.1:5000/players/Neymar
 
--- frontend/src/app/chart/chart.component.ts
+++ frontend/src/app/chart/chart.component.ts
@@ -7,39 +7,42 @@ import * as d3 from 'd3';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+  // Initialize variables with default values
   currentVariable: String = "Age";
   highlightValue: String = "randomString";
   highlightPlayer: Object;
   highlightPlayerName: String = "";
+  margin: Object = { top: 50, right: 20, bottom: 30, left: 40 };
 
+  // Grab what we need from the page load
   @ViewChild('chart')
     private chartContainer: ElementRef;
-
   @Input()
   data: Object;
 
-  margin = { top: 50, right: 20, bottom: 30, left: 40 };
-
+  // No additonal constructors or oninit command.
   constructor() { }
-
   ngOnInit() { }
 
+  // if this.data changes redraw the graph
   ngOnChanges(): void {
     if (!this.data) { return; }
-
     this.createChart(this.currentVariable, this.highlightValue, this.highlightPlayerName);
   }
 
+  // Redraw graph on page resize
   onResize() {
     this.createChart(this.currentVariable, this.highlightValue, this.highlightPlayerName);
   }
 
+  // Graph a vairiable on click
   public graphVariable(variable): void {
     this.currentVariable = variable;
     this.createChart(this.currentVariable, this.highlightValue, this.highlightPlayerName);
     this.highlight(this.highlightPlayer)
   }
 
+  // Highlight a specific bar on the graph on click
   public highlight(player) {
     this.highlightPlayer = player
     this.highlightPlayerName = player['Name']
@@ -49,51 +52,63 @@ export class ChartComponent implements OnInit {
 
   }
 
+  // Main create chart function
   private createChart(variable, highlightValue, highlightPlayerName): void {
 
+    // Remove the graph if it exists
     d3.select('svg').remove();
 
+    // Get the data and which element the graph will go in
     const element = this.chartContainer.nativeElement;
     const data = this.data;
 
+    // Summarize the table so the bars show correctly
     var summaryStats = d3.nest()
       .key(function(d) { return d[variable]; })
       .rollup(function(v) { return v.length; })
       .sortKeys(d3.ascending)
       .entries(data);
 
+    // Set height and width
     const svg = d3.select(element).append('svg')
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
 
+    // Set the chart size inside the element
     const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
     const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
 
+    // Set the x-axis scale
     const x = d3
       .scaleBand()
       .rangeRound([0, contentWidth])
       .padding(0.1)
       .domain(summaryStats.map(function(d) { return d.key }));
 
+    // Set the y axis scale
     const y = d3
       .scaleLinear()
       .rangeRound([contentHeight, 0])
       .domain([0, d3.max(summaryStats, d => d.value)]);
 
+    // Move the graph pane to the right position
     const g = svg.append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
+    // Append the name to the top of the graph if we're passed one
     g.append('text')
       .attr('x', 100)
       .attr('y', 0)
       .attr('text-anchor', 'middle')
       .text(highlightPlayerName)
 
+    // Set the x axis
     g.append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + contentHeight + ')')
       .call(d3.axisBottom(x));
 
+    // Set the y axis
     g.append('g')
       .attr('class', 'axis axis--y')
       .call(d3.axisLeft(y).ticks(10))
@@ -104,6 +119,7 @@ export class ChartComponent implements OnInit {
       .attr('text-anchor', 'end')
       .text('Frequency');
 
+    // Map the data to bars
     g.selectAll('.bar')
       .data(summaryStats)
       .enter().append('rect')
@@ -114,7 +130,7 @@ export class ChartComponent implements OnInit {
       .attr('height', d => contentHeight - y(d.value))
       .attr('fill', function (d) {
         if(d.key == highlightValue) {
-          return "blue";
+          return "#ADD8E6";
         } else {
           return "black";
         }
